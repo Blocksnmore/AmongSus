225b2fbcf43907623ea3b767d4aeaf4ef351544f
Blocksnmore <51055767+Blocksnmore@users.noreply.github.com>
2020-10-21 10:06:59 -0500

    Update devbot.js

--- devbot.js
+++ devbot.js
@@ -8,13 +8,26 @@ var mysqlc = mysql.createConnection({
     password : process.env.mysqlpassword,
     database : process.env.mysqldb});
    
-
+bot.on("message", async msg =>{
+    if(msg.author.bot) return;
+    var messageSplit = msg.content.toString().split(" ");
+    var command = messageSplit[0].substring(1);
+    switch(command){
+        case "help":
+            let embed = new discord.MessageEmbed().setTitle("Among sus help").setDescription("").setColor("#FF0000").setFooter("Thanks for using me in your discord server!");
+            try{
+            msg.channel.send(embed)
+            }catch(e){
+                msg.channel.send("I'm missing permission to `EMBED_LINKS` in this channel!");
+            }
+            break;
+    };
+});
 
 bot.on("ready", ()=>{
 var statusnum = 0; bot.user.setActivity(statuses[statusnum], {type: "WATCHING"}); statusnum++; setInterval(function(){
     if(statusnum -1 === statuses.length) statusnum = 0;
     bot.user.setActivity(statuses[statusnum], {type: "WATCHING"}); statusnum++; }, 60000);
 })
-
 
 bot.login(process.env.devtoken);
